a85ed56f1a99b3590c2e50e35b7dcffab3ad64b1
kevinkuruc <kkuruc@ou.edu>
2020-01-24 10:28:33 -0600

    Update totalfactorproductivity_component.jl

--- src_annualized/components/DICE/totalfactorproductivity_component.jl
+++ src_annualized/components/DICE/totalfactorproductivity_component.jl
@@ -5,14 +5,14 @@
     
     a0		= Parameter()				#Initial level of total factor productivity
 	ga0		= Parameter()				#Initial growth rate for TFP per 5 years
-	dela	= Parameter()				#Decline rate of TFP per 5 years
+	dela	= Parameter()				#Decline rate of TFP per 5 years (or year... inconsistency on Nordhaus spreadsheet)
 
     function run_timestep(p, v, d, t)
 		#Define function for GA
         if is_first(t)
-            v.GA[t] = log(p.ga0)/5 - 1  ##this is how to convert growth rates to annual levels
+            v.GA[t] = (1+p.ga0)^.2 - 1  ##this is how to convert growth rates to annual levels
         else
-            v.GA[t] = v.GA[t - 1] * exp(-p.dela)
+            v.GA[t] = v.GA[t - 1] * exp(-p.dela)  #dropped *5 in exponent (something seems odd here even on Nordhaus spreadsheet
         end
 		
 		#Define function for AL
